bd5cc860c58a3ea7043680279d1c94f50e8dc6ff
Dmytro Oliinyk <dmytro.oliinyk@ardas.biz>
2021-10-26 10:47:03 +0300

    Add redux devtools

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,11 @@
 import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 import reducer from './reducers';
 
-const store = createStore(reducer);
+const composeEnhancers = composeWithDevTools();
+
+const store = createStore(reducer, composeEnhancers);
 
 export default store;
 
