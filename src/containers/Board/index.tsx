4a2076014b41b93b52815337a39e45ce1e8e2a79
Dmytro Oliinyk <dmytro.oliinyk@ardas.biz>
2021-12-21 14:46:54 +0200

    Upload tasks when load app

--- src/containers/Board/index.tsx
+++ src/containers/Board/index.tsx
@@ -1,14 +1,22 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { Stack } from '@mui/material';
 
 import BoardColumn from 'containers/BoardColumn';
 
 import { taskTypes } from 'utils/constants';
 
+import { useActions } from 'store/hooks';
+import { fetchTasks } from 'store/actions/task';
+
 import useBoardStyles from './board.styles';
 
 const Board: FC = () => {
   const styles = useBoardStyles();
+
+  const uploadTasks = useActions(fetchTasks);
+  useEffect(() => {
+    uploadTasks();
+  });
 
   return (
     <Stack className={styles.board} direction="row" spacing={2}>
